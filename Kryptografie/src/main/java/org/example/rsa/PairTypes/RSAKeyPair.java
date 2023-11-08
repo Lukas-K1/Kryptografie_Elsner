c39ea9e3062a319cb19deb0c12013e7c024780de
Lukas-K1 <lukas.koepsel@freenet.de>
2023-11-08 20:04:38 +0100

    added key structures to improve code and maintainablility

---
+++ Kryptografie/src/main/java/org/example/rsa/PairTypes/RSAKeyPair.java
@@ -0,0 +1,19 @@
+package org.example.rsa.PairTypes;
+
+public class RSAKeyPair {
+    private final PublicKey _publicKey;
+    private final PrivateKey _privateKey;
+
+    public RSAKeyPair(PublicKey publicKey, PrivateKey privateKey) {
+        _publicKey = publicKey;
+        _privateKey = privateKey;
+    }
+
+    public PublicKey getPublicKey() {
+        return _publicKey;
+    }
+
+    public PrivateKey getPrivateKey() {
+        return _privateKey;
+    }
+}
